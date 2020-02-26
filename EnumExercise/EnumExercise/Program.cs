0dd3d5cda312da5aa621cdc8722502bab28f5872
ThomasShetler <tommyaasum22@gmail.com>
2020-02-26 12:10:20 -0800

    Enums

---
+++ EnumExercise/EnumExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("what day is it?:");
+            string value = Console.ReadLine();
+            try { 
+            daysOfTheWeek day = (daysOfTheWeek)Enum.Parse(typeof(daysOfTheWeek), value);
+                Console.WriteLine("Its " + value);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day");
+                Console.ReadLine();
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+    public enum daysOfTheWeek {
+        Sunday =1,
+        Monday=2,
+        Tuesday=3,
+        Wednesday=4,
+        Thurseday=5,
+        Friday=6,
+        Saturday=7
+    }
+
+
+}
