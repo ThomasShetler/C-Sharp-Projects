c79eadf917066282bb57bd7ba9f0841ca239b7d3
ThomasShetler <tommyaasum22@gmail.com>
2020-03-02 10:23:48 -0800

    ReeReeeREEEEE
    yeeto

---
+++ beautifulLambda/beautifulLambda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beautifulLambda
+{
+    class employee
+    {
+        private string first;
+        private string last;
+        private int id;
+        public employee(string first, string last, int id)
+        {
+            first = this.first;
+            last = this.last;
+            id = this.id;
+        }
+        public string First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+        public string Last
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private List<employee> employeeList = new List<employee>();
+        
+         
+    }
+
+}
