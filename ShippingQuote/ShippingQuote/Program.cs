2d18d568f42e34e81eaeacee7621d8af99793581
ThomasShetler <tommyaasum22@gmail.com>
2020-03-15 14:40:14 -0700

    ShippingQuote

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pkweight;
+            int pkwidth;
+            int pkheight;
+            int pklength;
+            double total;
+            Console.WriteLine("Welcome to Package Express.Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            pkweight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package width:");
+            pkwidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package lenght:");
+            pklength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height:");
+            pkheight = Convert.ToInt32(Console.ReadLine());
+
+            total = (pkweight * pkheight * pklength * pkwidth) / 100;
+            Console.WriteLine("Your estimated total for shipping this package is:" + total);
+            Console.ReadLine();
+        }
+    }
+}
