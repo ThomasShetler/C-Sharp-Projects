aebd23929b13997986b4eb9481a6405842bf2512
ThomasShetler <tommyaasum22@gmail.com>
2020-03-15 14:46:18 -0700

    do while statment

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool thing = false;
+            int i = 11;
+            while (!thing && (i > 10))
+            {
+
+                thing = true;
+            }
+            int a = 10;
+
+           
+            do
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a = a + 1;
+            }
+            while (a < 20);
+            Console.ReadLine();
+        }
+    }
+}
