c89f3619a5bb1ff11deea1cc13ab151d00ef4bb4
ThomasShetler <tommyaasum22@gmail.com>
2020-03-15 13:22:20 -0700

    Stringssssss

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hi = "hi";
+            string what = "whats";
+            string up = "up";
+
+            string yee = hi + what + up;
+
+            Console.WriteLine(yee);
+
+            StringBuilder sb = new StringBuilder("Hello ", 50);
+
+            sb.Append("World!!");
+            sb.AppendLine("Hello C#!");
+            sb.AppendLine("This is new line.");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
