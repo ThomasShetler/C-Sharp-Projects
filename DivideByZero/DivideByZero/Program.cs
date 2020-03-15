e752737cc477a437a606a738010a7311821379ed
ThomasShetler <tommyaasum22@gmail.com>
2020-03-15 15:41:47 -0700

    another drill

---
+++ DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.WriteLine("whats your age?");
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+                if (age < 0)
+                {
+                    throw new InvalidOperationException();
+
+                }
+
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("that is invail input! please put in a number greater then zero");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("that is invail input!");
+            }
+            
+            int age2;
+            int now;
+             now = DateTime.Now.Year;
+            age2 = now - age;
+            Console.WriteLine("You were born in:" + age2);  
+            Console.ReadLine();
+        }
+    }
+}
