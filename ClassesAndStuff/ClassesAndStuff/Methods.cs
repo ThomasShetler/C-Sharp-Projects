e56479ac1ac255384b7085c6a089a96cf01e0c6f
ThomasShetler <tommyaasum22@gmail.com>
2020-03-16 13:10:45 -0700

    updated classes and stuff

---
+++ ClassesAndStuff/ClassesAndStuff/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndStuff
+{
+    class Methods
+    {
+        public static int multiply(int i, int j)
+        {
+            int total = i * j;
+            return (total);
+        }
+        public static int sum(int i, int j)
+        {
+            int total = i + j;
+            return (total);
+        }
+        public static int sub(int i, int j)
+        {
+            int total = i - j;
+            return (total);
+        }
+    }
+}
--- ClassesAndStuff/ClassesAndStuff/Program.cs
+++ ClassesAndStuff/ClassesAndStuff/Program.cs
@@ -14,28 +14,15 @@ namespace ClassesAndStuff
             int firstSel = Convert.ToInt32(Console.ReadLine());
             int SecondSel = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("The 2 numbers multiplied is:");
-            Console.WriteLine(multiply(firstSel, SecondSel));
+            Console.WriteLine(Methods.multiply(firstSel, SecondSel));
             Console.WriteLine("The 2 numbers added is:");
-            Console.WriteLine(sum(firstSel, SecondSel));
+            Console.WriteLine(Methods.sum(firstSel, SecondSel));
             Console.WriteLine("subtracting the 2 numbers will leave you with:");
-            Console.WriteLine(sub(firstSel, SecondSel));
+            Console.WriteLine(Methods.sub(firstSel, SecondSel));
             Console.ReadLine();
 
         }
-        public static int multiply(int i, int j){
-            int total = i * j;
-            return (total);
-        }
-        public static int sum(int i, int j)
-        {
-            int total = i + j;
-            return (total);
-        }
-        public static int sub(int i, int j)
-        {
-           int total = i - j;
-            return (total);
-        }
+     
 
     }
 }
