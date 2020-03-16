5e1dbeb866fac2ab3c2c6500dd5953b39856a0cd
ThomasShetler <tommyaasum22@gmail.com>
2020-03-16 13:31:26 -0700

    Classes and stuff 2

---
+++ ClassesAndStuff2/ClassesAndStuff2/BasicMaths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndStuff2
+{
+    class BasicMaths
+    {
+        public static int mult(int a)
+        {
+            int total = a * 2;
+            return (total);
+        }
+        public static int sum(double a)
+        {
+            int total = Convert.ToInt32(a + 5);
+            return total;
+        }
+        public static int div(string a) {
+            int aa = Convert.ToInt32(a);
+            int total =aa / 2;
+            return (total);
+        }
+    }
+}
---
+++ ClassesAndStuff2/ClassesAndStuff2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndStuff2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number to divide");
+            try { 
+            string num1 = Console.ReadLine();
+            Console.WriteLine(BasicMaths.div(num1));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("that is invail input!");
+            }
+            finally { 
+
+            Console.WriteLine("Enter a number to add");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+             Console.WriteLine(BasicMaths.sum(num3));
+            Console.WriteLine("enter a number to multiply");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(BasicMaths.mult(num2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
