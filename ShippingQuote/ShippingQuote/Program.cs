962519e0cdf488ddb0bbaf8b0e61400c42500f5d
ThomasShetler <tommyaasum22@gmail.com>
2020-03-16 10:04:09 -0700

    shippqoute reuploaded

--- ShippingQuote/ShippingQuote/Program.cs
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -18,15 +18,31 @@ namespace ShippingQuote
             Console.WriteLine("Welcome to Package Express.Please follow the instructions below.");
             Console.WriteLine("Please enter the package weight:");
             pkweight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the package width:");
-            pkwidth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the package lenght:");
-            pklength = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the package height:");
-            pkheight = Convert.ToInt32(Console.ReadLine());
+            if (pkweight > 50)
+            {
+                Console.WriteLine("package must be less then 50 pound have a nice day!");
 
-            total = (pkweight * pkheight * pklength * pkwidth) / 100;
-            Console.WriteLine("Your estimated total for shipping this package is:" + total);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                pkwidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package lenght:");
+                pklength = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                pkheight = Convert.ToInt32(Console.ReadLine());
+                int dimensions = pklength + pkwidth + pklength;
+                if(dimensions > 50)
+                {
+                    Console.WriteLine("package is to big to be shipped!");
+                }
+                else
+                { 
+                total = (pkweight * pkheight * pklength * pkwidth) / 100;
+                Console.WriteLine("Your estimated total for shipping this package is:" + total);
+                }
+            }
+            
             Console.ReadLine();
         }
     }
