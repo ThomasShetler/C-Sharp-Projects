04b076e1c94b6e31e6e5394ec864d739c8d653e3
ThomasShetler <tommyaasum22@gmail.com>
2020-03-16 13:04:31 -0700

    Iteration updated
    yeeeeee

--- iterations/iterations/Program.cs
+++ iterations/iterations/Program.cs
@@ -13,7 +13,7 @@ namespace iterations
             string[] name = { "you", "sup", "howdy", "yussss", "REEEEEE" };
             Console.WriteLine("enter text:");
             string yourString = Console.ReadLine();
-            for (int i =0; i < (name.Length); i++)
+            for (int i = 0; i < (name.Length); i++)
             {
                 name[i] = (name[i] + yourString);
             }
@@ -24,6 +24,10 @@ namespace iterations
             Console.WriteLine("press enter");
             string stop = Console.ReadLine();
 
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("This assignment is pointless");
+            }
             while (stop == "")
             {
                 Console.WriteLine("type something to make it stop or press enter to never end");
@@ -54,21 +58,57 @@ namespace iterations
                 }
                
             }
-            List<string> stuff = new List<string> {"apple","house","dog","cat","snoodle"};
-            List<string> stuff2 = new List<string> { "orange", "appartment", "dog", "cat", "zombie" };
+            List<string> names2 = new List<string> { "bob", "dan", "rick", "phill", "james","bob", "bob", "jen", "julie" };
+            Console.WriteLine("type in a name to search");
+            string search2 = Console.ReadLine();
+            int indexer2 = 0;
+            bool found2 = false;
+            foreach (string people in names2)
+            {
+
+                if (search2 == people)
+                {
+                    found = true;
+                    Console.WriteLine("Your search result is at:" + indexer2 + " in the list ");
+                }
+                else if (found2 == false)
+                {
+                    found2 = false;
+                }
+                indexer2++;
+                if (indexer2 == names2.Count && found == false)
+                {
+                    Console.WriteLine("sorry we can't find that name");
+                }
+            }
+            
+            Console.ReadLine();
+            List<string> stuff = new List<string> {"apple","dog","house","dog","cat","cat","snoodle"};
+            int ii = 0;
+            int jj = 0;
             foreach (string things in stuff)
             {
-                foreach(string things2 in stuff2)
+                foreach(string things2 in stuff)
                 {
-                    if (things == things2)
+                    if (things == things2 && ii != jj)
                     {
-                        Console.WriteLine(things + " has appeared in the list");
+                        Console.WriteLine("index:" + ii + " (" + things + ") " + " was compared at index:" + jj + " (" + things2 + ") " + "which has appeared before!");
+                    }
+                    else if (ii != jj && things != things2)
+                    {
+                        Console.WriteLine("index:"+ ii + " ("+things+") " + " was compared at index:" + jj + " (" + things2 + ") " + "which is not a match..."); 
                     }
                     else
                     {
-                        Console.WriteLine(things + " has not appeared in this list");
+                        Console.WriteLine("index:" + ii + " (" + things + ") " + " was compared at index:" + jj + " (" + things2 + ") " + "which is the starting index and not another instance");
+                    }
+                    jj++;
+                    if (jj == stuff.Count)
+                    {
+                        jj = 0;
                     }
                 }
+                ii++;
             }
             Console.ReadLine();
 
