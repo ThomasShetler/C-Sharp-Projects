fa71d7d35967741fbe8a26e5db00480726d270e9
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-18 16:23:41 -0700

    DateTime drill DONE!

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("enter a number");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime hoursLater = now.AddHours(hours);
+            Console.WriteLine("It will be " + hoursLater + " in " + hours + " hours");
+            Console.ReadLine();
+        }
+    }
+}
