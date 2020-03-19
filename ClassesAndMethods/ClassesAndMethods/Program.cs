7788baa7d66ce21b266902588212e8818a2505f2
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-19 13:47:19 -0700

    yee

---
+++ ClassesAndMethods/ClassesAndMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("say a number");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Stuff stuff = new Stuff();
+
+           stuff.Yehaw(choice);
+           stuff.yee("bob");
+        }
+    }
+}
---
+++ ClassesAndMethods/ClassesAndMethods/Stuff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    static class stuff2 
+     {
+     }
+    class Stuff
+    {
+        public void yee()
+        {
+            Console.WriteLine("yee");
+        }
+        public void yee(string yee)
+        {
+            Console.WriteLine(yee);
+        }
+        public void Yehaw(int thingy)
+        {
+            int result = thingy / 2;
+            Console.WriteLine(result);
+        }
+
+    }
+
+}
