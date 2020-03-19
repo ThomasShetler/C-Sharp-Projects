558462438f111fd811f07aeb82ae3426a773dc61
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-19 11:33:02 -0700

    CONSTRUCTOR CHAINGINGINGINGINGING
    REEEEEE TODAY IS A GREAT DAYYYYY REEEEEE

---
+++ ConstructorChainging/ConstructorChainging/Constructors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainging
+{
+    class Constructors
+    {
+        string _studentType = "<Senior>";
+        string _id = "8";
+        string _fName = "Bob";
+        string _lName = "Smith";
+
+        public Constructors(string id)
+            : this(id, "Bob", "Smith")
+        {
+
+        }
+
+        public Constructors(string id, string fName)
+            : this(id, fName, "Smith")
+        {
+
+        }
+
+        public Constructors(string id, string fName, string lName)
+        {
+            //Validate logic.....
+            _studentType = "<Senior>";
+
+            _id = id;
+            _fName = fName;
+            _lName = lName;
+        }
+    }       
+     
+ 
+    
+
+
---
+++ ConstructorChainging/ConstructorChainging/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainging
+{
+    class Program 
+    { 
+    
+       
+        static void Main(string[] args)
+        {
+            const string namo = "THE BATTLE OF REEEEEEE"; 
+            //THIS IS THE CAINED CONSTRUCTOR OF POWEEEEERRRR REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+            Constructors obj = new Constructors("Thomas",7);
+            //THIS IS VAR VALUEEEEEE REEEEEEEEEEEEEEEEEEEEEEEE
+            var newDickie = new Dictionary<string, string>();
+                     
+
+
+        }
+    }
+}
