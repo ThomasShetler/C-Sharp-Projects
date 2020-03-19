42d97f33d09b457a3e6c89273d5bcb628d1126ad
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-19 12:45:11 -0700

    Super simple drill

---
+++ AccountingSystem/AccountingSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Acme Accounting Systems");
+            Console.WriteLine("Remeber, we're \"accounting\" on you! ");
+            Console.ReadLine();
+        }
+    }
+}
