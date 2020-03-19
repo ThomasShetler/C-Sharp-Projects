894fdbb0fec83122f0ecb1a3e5068b2ffb268048
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-19 12:33:27 -0700

    FileIO drill!

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Thomas Shetler\Documents\HTML\C-Sharp-Projects\FileIO\FileIO\NewFolder1\log.txt";
+            Console.WriteLine("Write a number!");
+            string text = Console.ReadLine();
+            //File.WriteAllText(path, text);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Thomas Shetler\Documents\HTML\C-Sharp-Projects\FileIO\FileIO\NewFolder1\log.txt", true))
+            {
+                file.WriteLine(text);
+            }
+            string readss = File.ReadAllText(@"C:\Users\Thomas Shetler\Documents\HTML\C-Sharp-Projects\FileIO\FileIO\NewFolder1\log.txt");
+            Console.WriteLine(readss);
+            
+        }
+    }
+}
