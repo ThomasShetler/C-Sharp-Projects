ddeb4859c870cab30d9c371133b5a5190695e565
Thomas Shetler <tommyaasum22@gmail.com>
2020-03-19 13:27:14 -0700

    Hourly rate Assignment

---
+++ HourlyRate/HourlyRate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HourlyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person 1 hourly rate");
+            int hourlyRate1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int hoursWorked1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2 hourly rate");
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
+
+            int total1 = (hourlyRate1 * hoursWorked1) * 52;
+            int total2 = (hourlyRate2 * hoursWorked2) * 52;
+
+            Console.WriteLine("Person 1 est Annual salary");
+            Console.WriteLine(total1);
+            Console.WriteLine("Person 2 Est Annual Salary");
+            Console.WriteLine(total2);
+            Console.WriteLine("Does person 1 make more then person 2");
+            if (total1 > total2)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
